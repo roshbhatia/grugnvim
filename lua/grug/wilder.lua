ac32e45bed944d037e1793332ce73d61a908c47d
Roshan Bhatia <rshnbhatia@gmail.com>
2024-07-12 15:42:54 -0700

    s

--- lua/grug/wilder.lua
+++ lua/grug/wilder.lua
@@ -1,1 +1,11 @@
-require('wilder').setup({modes = {':', '/', '?'}})
+local wilder = require('wilder')
+wilder.setup({
+    modes = {':', '/', '?'}
+})
+
+wilder.set_option('pipeline', {wilder.branch(wilder.cmdline_pipeline(), wilder.search_pipeline())})
+
+wilder.set_option('renderer', wilder.popupmenu_renderer({
+    -- highlighter applies highlighting to the candidates
+    highlighter = wilder.basic_highlighter()
+}))
