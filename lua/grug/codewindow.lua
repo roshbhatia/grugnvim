b1d2ee03bf5e98b3105b846fd3c38c64af07bf47
Roshan Bhatia <rshnbhatia@gmail.com>
2024-07-12 15:08:27 -0700

    Update codewindow.lua

--- lua/grug/codewindow.lua
+++ lua/grug/codewindow.lua
@@ -3,14 +3,4 @@ if vim.g.code_preview_enabled == nil then
     vim.g.code_preview_enabled = true  -- default to true
 end
 
-require('codewindow').setup({
-    auto_enable = function()
-        return vim.g.code_preview_enabled
-    end
-})
-
--- Apply default keybinds conditionally
-if vim.g.code_preview_enabled then
-    require('codewindow').apply_default_keybinds()
-end
-
+require('codewindow').setup()
