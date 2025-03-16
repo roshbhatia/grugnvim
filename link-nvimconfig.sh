5c1fc8ea03f859b6bd6e774c282d5c9012233479
Roshan Bhatia <rshnbhatia@gmail.com>
2025-03-15 18:02:14 -0700

    wip

--- link-nvimconfig.sh
+++ link-nvimconfig.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set the source directory where your Neovim configuration files are located
 src="$(pwd)"
@@ -21,3 +21,118 @@ fi
 # Create a symbolic link to the source directory
 ln -s "$src" "$dst"
 echo "Linked $src to $dst."
+
+# Create a separate directory for kubectl.nvim config
+kvdst="$HOME/.config/kv"
+mkdir -p "$kvdst"
+
+# Create init.lua for kubectl.nvim
+cat > "$kvdst/init.lua" <<EOL
+-- Initialize kubectl.nvim config
+vim.cmd('set nocompatible')
+vim.opt.viminfo:remove({'!'})
+
+-- Init lazy plugin manager
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable',
+                   lazypath})
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Install only necessary plugins for kubectl
+require('lazy').setup({
+    -- Core essentials
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'kepano/flexoki-neovim',
+    'bluz71/nvim-linefly',
+    -- Kubectl integration
+    'Ramilito/kubectl.nvim',
+    -- Helpful utilities
+    {
+        'nvim-telescope/telescope.nvim',
+        dependencies = {'nvim-lua/plenary.nvim'}
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = '*',
+        config = true
+    }
+}, {
+    lazy = false,
+    version = false
+})
+
+-- Basic settings
+vim.opt.termguicolors = true
+vim.cmd('colorscheme flexoki-dark')
+vim.wo.number = true
+vim.o.mouse = 'a'
+
+-- Kubectl setup with expanded capabilities
+require('kubectl').setup({
+    -- Configure kubectl settings here
+    -- These are examples - adjust to your needs
+    kubernetes_resources = {
+        "pods",
+        "deployments",
+        "services",
+        "configmaps",
+        "secrets",
+        "ingresses",
+        "namespaces",
+        "statefulsets",
+        "daemonsets",
+        "persistentvolumes",
+        "persistentvolumeclaims",
+    },
+    terminal_width_percentage = 0.8,
+    kubectl_terminal_height_ratio = 0.9, 
+    logs_formatter = "DLOG", -- or "auto"
+    logs_timestamp = true,
+    logs_format = "wide",  -- can be "default" or "wide"
+})
+
+-- Keybindings specific to kubectl
+vim.g.mapleader = ' '
+
+-- Kubectl main commands
+vim.api.nvim_set_keymap('n', '<leader>kp', ':lua require("kubectl").get_pods()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>kd', ':lua require("kubectl").get_deployments()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ks', ':lua require("kubectl").get_services()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ki', ':lua require("kubectl").get_ingresses()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>kc', ':lua require("kubectl").get_configmaps()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>kn', ':lua require("kubectl").get_namespaces()<CR>', { noremap = true, silent = true })
+
+-- Kubectl actions
+vim.api.nvim_set_keymap('n', '<leader>kl', ':lua require("kubectl").get_logs()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>kf', ':lua require("kubectl").port_forward()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ke', ':lua require("kubectl").exec()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>kk', ':lua require("kubectl").describe()<CR>', { noremap = true, silent = true })
+
+-- Telescope integration
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+
+-- Terminal
+vim.api.nvim_set_keymap('n', '<C-\\\\>', ':ToggleTerm<CR>', { noremap = true, silent = true })
+EOL
+
+echo "Created kubectl.nvim config at $kvdst"
+
+# Create a shell script for 'kv' command
+mkdir -p "$HOME/bin"
+cat > "$HOME/bin/kv" <<EOL
+#!/bin/bash
+
+# Start neovim with kubectl.nvim config
+XDG_CONFIG_HOME=~/.config NVIM_APPNAME=kv nvim "\$@"
+EOL
+
+# Make the script executable
+chmod +x "$HOME/bin/kv"
+
+echo "Created 'kv' command in ~/bin"
+echo "Make sure ~/bin is in your PATH or move the script to a directory in your PATH"
--- lua/grug/init.lua
+++ lua/grug/init.lua
@@ -27,7 +27,46 @@ require('lazy').setup({{
     end,
     opts = {}
 }, 'jackguo380/vim-lsp-cxx-highlight', 'liuchengxu/vim-which-key', 'mhinz/vim-startify', 'gorbit99/codewindow.nvim',
-                       'gelguy/wilder.nvim', 'bluz71/nvim-linefly'}, {
+'gelguy/wilder.nvim', 'bluz71/nvim-linefly',
+-- VSCode-like IDE features
+'neovim/nvim-lspconfig', -- LSP support
+'hrsh7th/nvim-cmp', -- Autocompletion
+'hrsh7th/cmp-nvim-lsp', -- LSP source for nvim-cmp
+'hrsh7th/cmp-buffer', -- Buffer source for nvim-cmp
+'hrsh7th/cmp-path', -- Path source for nvim-cmp
+'L3MON4D3/LuaSnip', -- Snippets engine
+'saadparwaiz1/cmp_luasnip', -- Snippets source for nvim-cmp
+'rafamadriz/friendly-snippets', -- Predefined snippets
+'onsails/lspkind.nvim', -- Add VS Code-like pictograms to completion menu
+'folke/trouble.nvim', -- Pretty diagnostics
+{
+    'nvim-telescope/telescope.nvim', -- Fuzzy finder
+    dependencies = {'nvim-lua/plenary.nvim'}
+},
+'simrat39/symbols-outline.nvim', -- Code outline/structure view
+{
+    'folke/which-key.nvim', -- Better keybindings display
+    event = 'VeryLazy',
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 300
+    end,
+},
+'windwp/nvim-autopairs', -- Auto pairing brackets
+'lukas-reineke/indent-blankline.nvim', -- Indent guide lines
+'terrortylor/nvim-comment', -- Easy commenting
+{
+    'akinsho/toggleterm.nvim', -- Better terminal
+    version = '*', 
+    config = true
+},
+-- Kubectl support
+{
+    'Ramilito/kubectl.nvim', -- Kubectl integration
+    dependencies = {
+        'nvim-lua/plenary.nvim'
+    }
+}}, {
     lazy = false,
     version = false
 })
@@ -38,6 +77,84 @@ local exile_modules = {'general', 'barline', 'startify', 'nvim-tree', 'codewindo
 for _, module in ipairs(exile_modules) do
     require('grug.' .. module)
 end
+
+-- LSP setup
+local lspconfig = require('lspconfig')
+
+-- Add your language servers here
+-- Example for common languages:
+-- lspconfig.tsserver.setup{}
+-- lspconfig.pyright.setup{}
+-- lspconfig.rust_analyzer.setup{}
+-- lspconfig.gopls.setup{}
+
+-- Autocompletion setup
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local lspkind = require('lspkind')
+
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { 'i', 's' }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'path' },
+    }),
+    formatting = {
+        format = lspkind.cmp_format({
+            mode = 'symbol_text',
+            maxwidth = 50,
+        })
+    }
+})
+
+-- Set up other IDE features
+require('nvim-autopairs').setup({})
+require('nvim_comment').setup()
+require('symbols-outline').setup()
+require('trouble').setup()
+require('indent_blankline').setup({
+    show_current_context = true,
+    show_current_context_start = true,
+})
+require('toggleterm').setup({
+    open_mapping = [[<c-\>]],
+    direction = 'float',
+})
+
+-- Kubectl setup
+require('kubectl').setup({
+    -- Your kubectl config here
+})
 
 -- Define a custom command to open Startify
 vim.cmd([[
@@ -56,6 +173,76 @@ vim.api.nvim_set_keymap('n', '<F1>', ':StartifyStart<CR>', {
 
 -- Map <F2> to open or close nvim-tree
 vim.api.nvim_set_keymap('n', '<F2>', ':NvimTreeToggle<CR>', {
+    noremap = true,
+    silent = true
+})
+
+-- Telescope keybindings
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', {
+    noremap = true,
+    silent = true
+})
+
+-- LSP keybindings
+vim.api.nvim_set_keymap('n', 'gd', ':lua vim.lsp.buf.definition()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', 'gr', ':lua vim.lsp.buf.references()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', 'gi', ':lua vim.lsp.buf.implementation()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', 'K', ':lua vim.lsp.buf.hover()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>rn', ':lua vim.lsp.buf.rename()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>ca', ':lua vim.lsp.buf.code_action()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>e', ':lua vim.diagnostic.open_float()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>tt', ':TroubleToggle<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>so', ':SymbolsOutline<CR>', {
+    noremap = true,
+    silent = true
+})
+
+-- Kubectl keybindings
+vim.api.nvim_set_keymap('n', '<leader>kl', ':lua require("kubectl").get_logs()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>kp', ':lua require("kubectl").get_pods()<CR>', {
+    noremap = true,
+    silent = true
+})
+vim.api.nvim_set_keymap('n', '<leader>kd', ':lua require("kubectl").describe()<CR>', {
     noremap = true,
     silent = true
 })
